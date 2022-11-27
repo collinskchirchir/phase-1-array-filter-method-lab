bb217744795bf9d944a2c1ea602a9fa2db14932d
Collins Chirchir <collinskchirchir@gmail.com>
2022-11-27 14:01:07 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution here
+// Sample Data
+const drivers = [
+   {
+     name: 'Bobby',
+     hometown: 'Pittsburgh' },
+   {
+     name: 'Sammy',
+     hometown: 'New York' } ,
+   {
+     name: 'Sally',
+     hometown: 'Cleveland' },
+   {
+     name: 'Annette',
+     hometown: 'Los Angeles' },
+   {
+     name: 'Bobby',
+     hometown: 'Tampa Bay' }
+ ];
+
+ function findMatching(collection, value)
+ {
+    return collection.filter(name => name.toLowerCase() == value.toLowerCase());
+}
+ function fuzzyMatch(collection, value)
+ {
+   return collection.filter(name => name.slice(0, 2) == value)
+}
+function matchName(collection, value)
+{
+   return collection.filter(driver => driver.name == value)
+}
